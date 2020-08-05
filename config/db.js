7f9a066a6a0e01469bda485831c8f3bbeb923f10
Mikhail Astashonak <thenaitrise@gmail.com>
2020-08-05 09:41:48 +0300

    05.08.2020 1

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('MongoDB connected...')
+    } catch(error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect to DB
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen( PORT, () => {
     console.log(`Server started on port ${PORT}`);
