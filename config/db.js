19106aed9ddf9f9e1aaebb72e8bc2bbf62a99f27
Mikhail Astashonak <thenaitrise@gmail.com>
2020-08-18 12:47:41 +0300

    18/08/2020 v2

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const dbString = require('../beautifulMessages/db');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(db, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
         console.log(dbString());
     } catch(error) {
         console.error(error.message);
--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const app = express();
 // Connect to DB
 connectDB();
 
+// Init Middleware 
+// This middleware gives us oportunity to 
+// read req.body data in more comfortable json object
+app.use(express.json({ extended: false }));
+
+// Checking if http (and server) works
 app.get('/', (req, res) => res.send('API running'));
+
+// Defining our PORT
 const PORT = process.env.PORT || 5000;
 
 // Define routes
@@ -16,6 +24,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+// Listen if our server starts
 app.listen( PORT, () => {
     console.log(portString(PORT));
 })
