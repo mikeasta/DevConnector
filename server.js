de1a18f7a2030432b68539490dd9f49d24b0d86d
Mikhail Astashonak <thenaitrise@gmail.com>
2020-08-30 20:21:20 +0300

    30/08/20

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+
 // Listen if our server starts
 app.listen( PORT, () => {
     console.log(portString(PORT));
-})
+});
